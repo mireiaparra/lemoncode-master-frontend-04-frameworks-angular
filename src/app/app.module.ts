028673ed351e1319dc9a77590f8699133882d6bc
mireia <mirmalparra@gmail.com>
2023-09-08 10:35:12 +0200

    Añadir componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderPublicComponent } from './layout/header-public/header-public.component';
+import { HeaderPrivateComponent } from './layout/header-private/header-private.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuPublicComponent } from './layout/menu-public/menu-public.component';
+import { MenuPrivateComponent } from './layout/menu-private/menu-private.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPublicComponent,
+    HeaderPrivateComponent,
+    FooterComponent,
+    MenuPublicComponent,
+    MenuPrivateComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/menu-private/menu-private.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-private',
+  templateUrl: './menu-private.component.html',
+  styleUrls: ['./menu-private.component.css']
+})
+export class MenuPrivateComponent {
+
+}
