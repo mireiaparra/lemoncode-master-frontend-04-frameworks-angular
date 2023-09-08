c429f378bd8c37354e965dd49f2474d25354f8c1
mireia <mirmalparra@gmail.com>
2023-09-08 11:05:37 +0200

    Añadir rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'profile', component: ProfileComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderPublicComponent } from './layout/header-public/header-public.component';
-import { HeaderPrivateComponent } from './layout/header-private/header-private.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { MenuPublicComponent } from './layout/menu-public/menu-public.component';
-import { MenuPrivateComponent } from './layout/menu-private/menu-private.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AboutComponent } from './pages/about/about.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderPrivateComponent } from './layout/header-private/header-private.component';
+import { HeaderPublicComponent } from './layout/header-public/header-public.component';
+import { MenuPrivateComponent } from './layout/menu-private/menu-private.component';
+import { MenuPublicComponent } from './layout/menu-public/menu-public.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -25,12 +30,17 @@ import { GalleryComponent } from './pages/gallery/gallery.component';
     HomeComponent,
     LoginComponent,
     AboutComponent,
-    GalleryComponent
+    GalleryComponent,
+    DashboardComponent,
+    CrudComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
